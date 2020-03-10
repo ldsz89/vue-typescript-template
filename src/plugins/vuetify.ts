3eb0ebf140ad7ea181b32757c3b242466eebbb7e
Dre Shannon <leandre.shannon@centurylink.com>
2020-03-10 09:21:24 -0500

    initial commit

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import 'vuetify/dist/vuetify.min.css';
+import {lightTheme, darkTheme} from '../theme';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+  theme: {
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    options: {
+      customProperties: true,
+    },
+  },
+});
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import {VuetifyThemeVariant} from 'vuetify/types/services/theme';
+
+export const lightTheme: VuetifyThemeVariant = {
+  primary: '#34B233',
+  secondary: '#0047bb',
+  accent: '#48d597',
+  error: '#b50D12',
+  info: '#0047bb',
+  success: '#097a49',
+  warning: '#a66102',
+};
+
+export const darkTheme: VuetifyThemeVariant = {
+  primary: '#00853f',
+  secondary: '#001e60',
+  accent: '#097a49',
+  error: '#c9434e',
+  info: '#007199',
+  success: '#2e7d32',
+  warning: '#ffb143',
+};
